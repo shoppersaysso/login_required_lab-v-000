5195f60b1999528ba7f746e53be0f8696cd3fc50
Sara Hopper <sara.hopper@gmail.com>
2017-08-09 15:19:43 +0000

    automatically backed up by learn

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   # root 'welcome#index'
   root 'index'
 
-  get 'secrets#show'
+  get 'secrets/show' => 'secrets#show'
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
