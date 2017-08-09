a6effae2197940d81a85d72e23f01640fbef53e7
Sara Hopper <sara.hopper@gmail.com>
2017-08-09 15:39:45 +0000

    automatically backed up by learn

--- app/controllers/secrets_controller.rb
+++ app/controllers/secrets_controller.rb
@@ -2,11 +2,6 @@ class SecretsController < ApplicationController
   before_action :require_login
 
   def show
-    if current_user
-      render :show
-    else
-      redirect_to controller: 'sessions', action: 'new'
-    end
   end
 
 
