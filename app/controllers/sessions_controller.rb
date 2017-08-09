ccc4a937946cd8b6591cc5c6e3a0a3ab0136f523
Sara Hopper <sara.hopper@gmail.com>
2017-08-09 15:04:21 +0000

    automatically backed up by learn

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,22 @@
+class SessionsController < ApplicationController
+
+  def current_user
+
+  end
+
+  def new
+  end
+
+  def create
+    return redirect_to(controller: 'sessions',
+                       action: 'new') if !params[:name] || params[:name].empty?
+    session[:name] = params[:name]
+    redirect_to controller: 'application', action: 'hello'
+  end
+
+  def destroy
+    session.delete :name
+  end
+
+
+end
