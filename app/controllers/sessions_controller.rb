e7dbf8e85235114a529758db90576f0eeb78ce6f
Sara Hopper <sara.hopper@gmail.com>
2017-08-09 15:28:12 +0000

    automatically backed up by learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,7 +7,7 @@ class SessionsController < ApplicationController
     return redirect_to(controller: 'sessions',
                        action: 'new') if !params[:name] || params[:name].empty?
     session[:name] = params[:name]
-    redirect_to controller: 'application', action: 'hello'
+    redirect_to controller: 'sessions', action: 'new'
   end
 
   def destroy
