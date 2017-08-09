284506b00a8a44c905697dc420bc71dd0f5b5ddd
Sara Hopper <sara.hopper@gmail.com>
2017-08-09 15:14:20 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def current_user
-    @current_user = (session[:name] ||= [])
+    session[:name]
   end
 
 end
